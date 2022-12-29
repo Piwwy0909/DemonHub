0a6a7c7750f81d7b70ae6bd215ac98a0d99f8f5a
Piwwy0909 <121379119+Piwwy0909@users.noreply.github.com>
2022-12-29 19:54:51 +0700

    Update Pls_Donateme_Luraph_500Bath.lua

--- Pls_Donateme_Luraph_500Bath.lua
+++ Pls_Donateme_Luraph_500Bath.lua
@@ -1,5 +1,11 @@
 ([[
-  
+                This script has been licensed using Luarmor
+            Unauthorized distribution of this script is forbidden.
+      Any attempts at tampering, reverse engineering or modifying this script's 
+      internal logic will result in a global ban, and make you blacklisted from
+            every single script that has been licensed with Luarmor
+
+        Luarmor v3 for Roblox, #1 lua whitelisting system by Federal#9999
 				   https://luarmor.net/
 
       ____                               _   _       _     
